05c6c8601e3a40717d485a93ea0a8da9b67f6e4b
Nathan Roe <nate1234zach@gmail.com>
2020-12-17 17:18:34 -0700

    added a findConnectionSpace function to graph

--- data_structures/graph/graph.py
+++ data_structures/graph/graph.py
@@ -24,13 +24,35 @@ class Graph:
             if node.first_name == first and node.last_name == last:
                 return node
         return None
+    def findConnectionSpace(self, user, potCon, count = 0):
+        if count == 3:
+            return count
+        if potCon in user.connections:
+            return count
+        else:
+            for u in user.connections:
+                return self.findConnectionSpace(u, potCon, count+1)
+
 g = Graph()
 g.addNode("nate", "bob")
 g.addNode("nateasdf", "boasdfb")
 g.addNode("natfdasde", "bobdfdsa")
 g.addNode("logan", "bob")
+g.addNode("natfdasdfasde", "boasdfbdfdsa")
+g.addNode("natfdaasdfsdfasde", "boasdfbdasdffdsa")
 g.printNodes()
 this_node = g.getNode("nate", "bob")
 con_node = g.getNode("logan", "bob")
-this_node.addConnection(con_node)
+c1 = g.getNode("nateasdf", "boasdfb")
+c2 = g.getNode("natfdasde", "bobdfdsa")
+c3 = g.getNode("natfdasdfasde", "boasdfbdfdsa")
+c4 = g.getNode("natfdaasdfsdfasde", "boasdfbdasdffdsa")
+# this_node.addConnection(con_node)
+con_node.addConnection(c1)
+this_node.addConnection(c1)
+c1.addConnection(c2)
+c2.addConnection(c3)
+c3.addConnection(c4)
+c4.addConnection(con_node)
 this_node.printConnections()
+print(g.findConnectionSpace(this_node, con_node))
