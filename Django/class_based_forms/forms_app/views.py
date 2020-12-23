681fb2bb6060dc5e3a46ec6b849a6f6b36deb24f
Nathan Roe <nate1234zach@gmail.com>
2020-12-22 21:56:39 -0700

    added working form pre-ajax

--- Django/class_based_forms/forms_app/views.py
+++ Django/class_based_forms/forms_app/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .components.form import RegisterForm as rf
+from .models import User
 # Create your views here.
 def index(req):
-    context = {"reg_form":rf}
+    allUsers = User.objects.all()
+    context = {"reg_form":rf, "users":allUsers}
     return render(req, "index.html", context)
 def process(req):
     if req.method == "POST":
         showForm = rf(req.POST)
         if showForm.is_valid():
-            return redirect("success")
+            this_user = User.objects.create(
+                first_name=req.POST["first_name"],
+                last_name=req.POST["last_name"],
+                email=req.POST["email"],
+                password=req.POST["password"],
+                confirm_password=req.POST["confirm_password"]
+            )
+            this_user.save()
+            req.session["uuid"] = this_user.id
+            context = {
+                "users":User.objects.all()
+            }
+            return render(req, "index.html", context)
         else:
-            return redirect("index.html")
+            print("Something went wrong")
+            return redirect("/")
+def success(req):
+    this_user = User.objects.get(id=req.session["uuid"])
+    context = {"user":this_user}
+    return render(req, "success.html", context)
+        
